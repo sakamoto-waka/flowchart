e764062ebb4d07fd1eda2326d923f6a1e736080b
EC2 Default User <ec2-user@ip-172-31-32-12.ec2.internal>
2022-02-05 02:10:34 +0000

    finish: f-6

--- air_conditioner.rb
+++ air_conditioner.rb
@@ -2,6 +2,7 @@ input_lines = readlines
 
 room = input_lines[0].to_i
 air_conditioner = input_lines[1].to_i
+wind = input_lines[2].to_i
 
 tempreture_deff = (room - air_conditioner).abs
 
@@ -14,5 +15,25 @@ elsif tempreture_deff >= 5 && tempreture_deff < 10
 elsif tempreture_deff >= 10
   require_time = 60
 end
+
+if wind == 1
+  require_time = require_time
+elsif wind == 2
+  require_time = require_time - 5
+elsif wind == 3
+  require_time = require_time - 10
+else
+  p '風量は1~3にしてください'
+  exit
+end
+
+#case = wind
+  #when 1 require_time
+  #when 2 require_time = require_time - 5
+  #when 3 require_time = require_time - 10
+  #else 
+  #p '風量は1~3にしてください'
+  #exit
+#end
 
 p require_time
